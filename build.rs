1f6c8cdbc7fc9cfda524138a90af2a6ed12babae
Satoshi Moriai <satoshi.moriai@gmail.com>
2019-12-08 16:18:51 +0900

    Use i386-elf-gcc for C-compiler and linker.

--- build.rs
+++ build.rs
@@ -2,9 +2,13 @@ use std::env;
 use std::fs::File;
 use std::io::Write;
 use std::path::PathBuf;
+use std::process::Command;
 
 fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     File::create(out_dir.join("com.ld")).unwrap().write_all(include_bytes!("com.ld")).unwrap();
-    File::create(out_dir.join("startup.o")).unwrap().write_all(include_bytes!("startup.o")).unwrap();
+    Command::new("i386-elf-gcc")
+        .args(&["startup.c", "-c", "-march=i386", "-m16", "-ffreestanding", "-fno-pie", "-o"])
+        .arg(out_dir.join("startup.o"))
+        .status().unwrap();
 }
